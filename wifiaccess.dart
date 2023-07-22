686c36d762fa38e59cf015ff41ebad6fc21ef36f
davisniranjan <119030112+davisniranjan@users.noreply.github.com>
2023-07-22 15:57:38 +0530

    Create wifiaccess.dart

---
+++ wifiaccess.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WifiUserVerificationApp());
+}
+
+class WifiUserVerificationApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    // Implement your login logic here
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // For simplicity, let's assume valid credentials grant Wi-Fi access
+    if (isValidCredentials(username, password)) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WifiAccessPage()),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Invalid username or password.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  bool isValidCredentials(String username, String password) {
+    // Implement your authentication logic here
+    // For this example, we will accept any non-empty username and password
+    return username.isNotEmpty && password.isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wi-Fi User Verification')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Verify User'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WifiAccessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wi-Fi Access')),
+      body: Center(
+        child: Text('You are now connected to Wi-Fi.'),
+      ),
+    );
+  }
+}
